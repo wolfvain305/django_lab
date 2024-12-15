22e39b6d5070fac03986cfc63173321dcb23c90b
DrueWolf <dwolf0305@gmail.com>
2024-12-14 21:56:44 -0500

    mor eof part 3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("polls/", include("polls.urls")),
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
